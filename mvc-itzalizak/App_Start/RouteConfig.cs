3cee2ca3f9dcbb4135298e9cf1a3692d5fe9f995
ikerib <ikerib@gmail.com>
2015-02-18 09:49:09 +0100

    First commit

---
+++ mvc-itzalizak/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace mvc_itzalizak
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "itzali",
+                url:"itzali",
+                defaults: new {  controller ="Home", action = "Itzali"}
+                );
+        }
+    }
+}
---
+++ mvc-itzalizak/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace mvc_itzalizak.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public void Get()
+        //public IEnumerable<string> Get()
+        {
+            string arg = "/s /f /t 0";
+
+            System.Diagnostics.Process.Start("shutdown", arg);
+        }
+    }
+}
